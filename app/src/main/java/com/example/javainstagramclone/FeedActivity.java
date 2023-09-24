212b6a291b25bdccdeee801d2697566290bd3f4e
ozcanbayram <ozzcanbayram@gmail.com>
2023-09-24 18:58:50 +0300

    User interface and layouts were created.

--- app/src/main/java/com/example/javainstagramclone/FeedActivity.java
+++ app/src/main/java/com/example/javainstagramclone/FeedActivity.java
@@ -3,22 +3,12 @@ package com.example.javainstagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_feed);
     }
-
-    public void signInClicked(View view){
-
-    }
-
-    public void signUpClicked(View view){
-
-    }
-
 }
--- app/src/main/java/com/example/javainstagramclone/UploadActivity.java
+++ app/src/main/java/com/example/javainstagramclone/UploadActivity.java
@@ -5,20 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload);
     }
 
-    public void signInClicked(View view){
+    public void uploadButtonClicked(View view){
 
     }
 
-    public void signUpClicked(View view){
-
+    public void selectImage(View view){
+        
     }
 
 }
