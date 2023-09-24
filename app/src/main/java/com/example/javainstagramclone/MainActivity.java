b6f4a868abcf56a8ec11445fc7f71ed26e2d0015
ozcanbayram <ozzcanbayram@gmail.com>
2023-09-24 21:30:26 +0300

    Sign in prodectures have been complated.

--- app/src/main/java/com/example/javainstagramclone/MainActivity.java
+++ app/src/main/java/com/example/javainstagramclone/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,9 +30,38 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance(); // Initialize Firebase Auth
 
+        FirebaseUser user = auth.getCurrentUser();
+        if(user != null){
+            Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     public void signInClicked(View view){
+
+        String email = binding.emailText.getText().toString();       //Get the email with viewBinding
+        String password = binding.passwordText.getText().toString(); //Get the password with viewBinding
+
+        if(email.equals("")||password.equals("")){  //if there is empty informations
+            Toast.makeText(this, "Please enter email and password.", Toast.LENGTH_SHORT).show();
+        }else {
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() { //if successfull
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                    Toast.makeText(MainActivity.this, "Sign in is succesfull.", Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {   //if fail (not successfull)
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
     }
 
