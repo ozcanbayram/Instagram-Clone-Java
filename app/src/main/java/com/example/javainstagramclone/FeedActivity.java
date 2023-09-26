7aeb37d3ef2205374ba81e6e8354c4970a29d2d9
ozcanbayram <ozzcanbayram@gmail.com>
2023-09-26 22:44:55 +0300

    Extract data from Cloud Firestore.

--- app/src/main/java/com/example/javainstagramclone/FeedActivity.java
+++ app/src/main/java/com/example/javainstagramclone/FeedActivity.java
@@ -1,6 +1,7 @@
 package com.example.javainstagramclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,10 +12,22 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class FeedActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;  // Add Firebase  Authentication (name: Auth)
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,38 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
 
         auth = FirebaseAuth.getInstance(); //İnitilaze FirebaseAuth
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getData();
+    }
+
+    private void getData(){
+
+        //DocumentReference documentReference = firebaseFirestore.collection("Posts").document("sadas");
+        //CollectionReference collectionReference = firebaseFirestore.collection("Posts").document("adsad");
+
+        firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(error!=null){
+                    Toast.makeText(FeedActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                if(value!=null){
+                    for(DocumentSnapshot snapshot : value.getDocuments()){
+                        Map<String, Object> data = snapshot.getData();
+
+                        //Casting
+                        String userEmail = (String) data.get("userEmail");
+                        String comment = (String) data.get("comment");
+                        String downloadUrl = (String) data.get("downloadurl");
+                        
+                    }
+                }
+
+            }
+        });
+
     }
 
     @Override
